a4ea1b38fcccecba5a49d2e85799966bf3d5d392
Dustin200168214 <200168214@student.georgianc.on.ca>
2019-09-20 10:54:35 -0400

    Completed Lab 2

---
+++ Lab2/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(string name)
+        {
+            // Add product to our view bag container
+            ViewBag.category = "You selected category: " + name;
+            return View();
+        }
+    }
+}
